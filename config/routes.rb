d460743f2f9036b2e96ce738e126857bdbfbbb60
Carlijn Driessen <carlijndriessen.dev@gmail.com>
2025-05-20 14:23:22 +0200

    Challenge completed except formatting optional

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :tasks, only: [:index, :new, :show, :create, :edit, :update]
+
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
